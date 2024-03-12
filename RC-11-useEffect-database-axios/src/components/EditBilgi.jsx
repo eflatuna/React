d9cfb7dc1bfdfe4c1edc1613970cb19ae69f9c77
esra <esrabaskaya84@gmail.com>
2024-03-12 11:03:10 +0100

    updated

--- RC-11-useEffect-database-axios/src/components/EditBilgi.jsx
+++ RC-11-useEffect-database-axios/src/components/EditBilgi.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const EditBilgi = ({ item, setItem, getTutorials }) => {
-	const { id, title, description } = item;
+	const { id, title: eskiTitle, description: eskiDesc } = item;
+	const [title, setTitle] = useState(eskiTitle);
+	const [description, setDescription] = useState(eskiDesc);
+
+	console.log(title, description);
+	//? https://react.dev/reference/react/useState#usestate
+	//! State degiskeninin degeri, 1.render ile initialState
+	//! parametresinin ilk degerini alir. Dolayisiyle bu durumda
+	//! prop'tan gelen ilk deger state'e aktarilir.
+	//! Sonradan degisen props degerleri useState'e aktarilmaz.
+	//! Eger props'tan gelen degerleri her degisimde useState'e
+	//! aktarmak istersek useEffect hook'unu componentDidUpdate
+	//! gibi kullanabiriz.
+
+	//* componentDidUpdate => gelişim/güncelleme
+	useEffect(() => {
+		setTitle(eskiTitle);
+		setDescription(eskiDesc);
+	}, [eskiDesc, eskiTitle]);
 
 	const url = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
 
+	// const putTutorial = async () => {
+	//   try {
+	//     await axios.put(`${url}${id}/`, { title, description });
+	//     getTutorials();
+	//   } catch (error) {
+	//     console.log(error);
+	//   }
+	// };
 	const putTutorial = async (eleman) => {
-		await axios.put(`${url}${eleman.id}/`, eleman);
-
-		getTutorials();
+		try {
+			await axios.put(`${url}${eleman.id}/`, eleman);
+			getTutorials();
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return (
@@ -43,6 +72,7 @@ const EditBilgi = ({ item, setItem, getTutorials }) => {
 								className="form-control"
 								id="title"
 								value={title || ""}
+								// onChange={(e) => setTitle(e.target.value)}
 								onChange={(e) =>
 									setItem({ ...item, title: e.target.value })
 								}
@@ -55,6 +85,7 @@ const EditBilgi = ({ item, setItem, getTutorials }) => {
 								id="desc"
 								className="form-control"
 								value={description || ""}
+								// onChange={(e) => setDescription(e.target.value)}
 								onChange={(e) =>
 									setItem({
 										...item,
