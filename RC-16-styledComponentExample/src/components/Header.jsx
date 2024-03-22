bfa6152f6f47a18a7c247fae14c99fac0420f5eb
esra <esrabaskaya84@gmail.com>
2024-03-22 11:20:52 +0100

    props

--- RC-16-styledComponentExample/src/components/Header.jsx
+++ RC-16-styledComponentExample/src/components/Header.jsx
@@ -1,9 +1,32 @@
-
+import ImageSSS, { LogoSSS } from "./styles/ImageSSS";
+import DisplaySSS from "./styles/DisplaySSS";
+import ButtonSSS, { DetayButon } from "./styles/ButonSSS";
 
 const Header = () => {
-  return (
-   <div>header</div>
-  );
+	return (
+		<div>
+			<DisplaySSS>
+				<LogoSSS src="./images/logo.png" alt="" />
+			</DisplaySSS>
+			<DisplaySSS>
+				<ButtonSSS kadir="res">Apply Courses</ButtonSSS>
+				<ButtonSSS omer>Talk to Adviser</ButtonSSS>
+			</DisplaySSS>
+			<div>
+				<div>
+					<h1>The IT Career of Your Dreams Starts Here</h1>
+					<p>
+						Clarusway is a leading international software Bootcamp.
+						Join a micro class online with other trainees and learn
+						coding skills with a highly-skilled instructor.
+					</p>
+					<ButtonSSS omer>Start Your New Career</ButtonSSS>
+					<DetayButon>Details</DetayButon>
+				</div>
+				<ImageSSS src="./images/hero.jpg" alt="" />
+			</div>
+		</div>
+	);
 };
 
 export default Header;
