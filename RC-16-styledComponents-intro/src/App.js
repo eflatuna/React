db1d3257c311d663e10d9c47e7276cfb3bbae8e7
esra <esrabaskaya84@gmail.com>
2024-03-22 10:12:49 +0100

    example

--- RC-16-styledComponents-intro/src/App.js
+++ RC-16-styledComponents-intro/src/App.js
@@ -1,14 +1,12 @@
-import "./App.css";
-
 import React from "react";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <div>
-     
-      App
-    </div>
-  );
+	return (
+		<div>
+			<Home />
+		</div>
+	);
 }
 
 export default App;
