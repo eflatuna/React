cca618270a171f6545c1c120475326c2b5f33a1a
esra <esrabaskaya84@gmail.com>
2024-03-27 09:48:12 +0100

    about

--- RC-17-18-RecipeAppProject copy/src/components/navbar/Navbar.jsx
+++ RC-17-18-RecipeAppProject copy/src/components/navbar/Navbar.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import {  Hamburger, Logo, Menu, MenuLink, Nav } from './NavbarStyles'
+import React from "react";
+import { Hamburger, Logo, Menu, MenuLink, Nav } from "./NavbarStyles";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from 'react';
+import { useState } from "react";
 const Navbar = () => {
+	const [open, setOpen] = useState(false);
 
+	return (
+		<Nav>
+			<Logo to="/home" onClick={() => setOpen(false)}>
+				<i>{"<Clarusway/>"} </i>
+				<span>recipe</span>
+			</Logo>
 
-  return (
-    <Nav>
-      <Logo to="/home">
-        <i>{"<Clarusway/>"} </i>
-        <span>recipe</span>
-      </Logo>
+			<Hamburger onClick={() => setOpen(!open)}>
+				<GiHamburgerMenu />
+			</Hamburger>
 
-      <Hamburger >
-        <GiHamburgerMenu />
-      </Hamburger>
+			<Menu open={open} onClick={() => setOpen(false)}>
+				<MenuLink to="/about"> about</MenuLink>
+				<a href="https://github.com/" target="blank">
+					github
+				</a>
+				<MenuLink to="/">logout</MenuLink>
+			</Menu>
+		</Nav>
+	);
+};
 
-      <Menu >
-        <MenuLink to="/about"> about</MenuLink>
-        <a href="https://github.com/" target='blank'>github</a>
-        <MenuLink to="/">logout</MenuLink>
-      </Menu>
-    </Nav>
-  );
-}
-
-export default Navbar
+export default Navbar;
--- RC-17-18-RecipeAppProject copy/src/router/AppRouter.jsx
+++ RC-17-18-RecipeAppProject copy/src/router/AppRouter.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import Login from "../pages/login/Login";
 import Home from "../pages/home/Home";
+import About from "../pages/about/About";
 import PrivateRouter from "./PrivateRouter";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
 	return (
 		<BrowserRouter>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Login />} />
 
 				<Route path="/home" element={<PrivateRouter />}>
 					<Route path="/home" element={<Home />} />
 				</Route>
+				<Route path="about" element={<About />} />
 			</Routes>
 		</BrowserRouter>
 	);
