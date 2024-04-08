98acb3202b0b673b2f49812771468157c33fb9fa
esra <esrabaskaya84@gmail.com>
2024-04-08 11:59:16 +0200

    reducer

---
+++ RC-22-redux-intro/inclass/src/redux/reducers/counterReducer.js
@@ -0,0 +1,21 @@
+// const [count,setCount]=useState()
+// const [todos,setTodos]=useState()
+
+const initialState = {
+	count: 10,
+};
+
+export const counterReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "INC":
+			return { count: state.count + 1 };
+		case "DEC":
+			return { count: state.count - 1 };
+		case "RESET":
+			// return initialState;
+			return { ...state, count: 0 }; //!birden fazla olursa böyle yazariz
+		default:
+			return state; //state in o anki halini geri dönüyor eger degisiklik yapilmadiysa,yani olmayan bir typw bilgisi geldiginde da state i oldugu gibi döndürür.
+	}
+	//!Reducer fonksiyonu mutlaka bir obje döndürür
+};
