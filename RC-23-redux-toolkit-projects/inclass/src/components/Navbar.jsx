fd163fa26b36caa7de2de38c76c3f56e2a67fb05
esra <esrabaskaya84@gmail.com>
2024-04-09 12:44:37 +0200

    login_logout

--- RC-23-redux-toolkit-projects/inclass/src/components/Navbar.jsx
+++ RC-23-redux-toolkit-projects/inclass/src/components/Navbar.jsx
@@ -10,79 +10,99 @@ import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutSuccess } from "../features/authSlice";
 
 function ResponsiveAppBar() {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const user = useSelector((state) => state.auth.user);
+	const dispatch = useDispatch();
 
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
 
-  return (
-    <AppBar
-      position="static"
-      color="morcivert"
-      sx={{ backgroundColor: "morcivert.dark" }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <BusinessIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-              flexGrow: 1,
-              cursor: "pointer",
-            }}
-          >
-            Clarusway - DE08
-          </Typography>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar>A</Avatar>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem>
-                <Typography textAlign="center">Login</Typography>
-              </MenuItem>
-              <MenuItem>
-                <Typography textAlign="center">Logout</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+	const handleLogout = () => {
+		dispatch(logoutSuccess());
+	};
+
+	return (
+		<AppBar
+			position="static"
+			color="morcivert"
+			sx={{ backgroundColor: "morcivert.dark" }}
+		>
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<BusinessIcon
+						sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+					/>
+					<Typography
+						variant="h6"
+						noWrap
+						component="a"
+						href="/"
+						sx={{
+							mr: 2,
+							display: { xs: "none", md: "flex" },
+							fontFamily: "monospace",
+							fontWeight: 700,
+							letterSpacing: ".3rem",
+							color: "inherit",
+							textDecoration: "none",
+							flexGrow: 1,
+							cursor: "pointer",
+						}}
+					>
+						Clarusway - DE08
+					</Typography>
+					<Box sx={{ flexGrow: 0 }}>
+						<Tooltip title="Open settings">
+							<IconButton
+								onClick={handleOpenUserMenu}
+								sx={{ p: 0 }}
+							>
+								<Avatar>{user?.email[0]}</Avatar>
+							</IconButton>
+						</Tooltip>
+						<Menu
+							sx={{ mt: "45px" }}
+							id="menu-appbar"
+							anchorEl={anchorElUser}
+							anchorOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							open={Boolean(anchorElUser)}
+							onClose={handleCloseUserMenu}
+						>
+							{user?.email ? (
+								<MenuItem onClick={handleLogout}>
+									<Typography textAlign="center">
+										Logout
+									</Typography>
+								</MenuItem>
+							) : (
+								<MenuItem>
+									<Typography textAlign="center">
+										Login
+									</Typography>
+								</MenuItem>
+							)}
+						</Menu>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
 export default ResponsiveAppBar;
